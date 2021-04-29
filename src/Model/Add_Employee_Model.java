afd15e9d8a8932c7c1fb2eff5b3133e69167a5f7
MustafizurRman <83420606+MustafizurRman@users.noreply.github.com>
2021-04-30 02:38:41 +0600

    Add files via upload

---
+++ src/Model/Add_Employee_Model.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Controller.conn;
+
+public class Add_Employee_Model {
+
+    public static boolean add_employee (String a,String bb,String c,String d,String e,String ff,String g,String h,String i,String j,String k){
+        try{
+            Controller.conn cc = new conn();
+            String q = "insert into employee values('"+a+"','"+bb+"','"+c+"','"+d+"','"+e+"','"+ff+"','"+g+"','"+h+"','"+i+"','"+j+"','"+k+"')";
+            cc.s.executeUpdate(q);
+            return true;
+
+        }
+
+        catch (Exception ee) {
+            ee.printStackTrace();
+        }
+        return false;
+
+
+    }
+}
